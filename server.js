e7fecc362c749dd5d2513978b41c4da88168ed7d
Juan C Olmedo <doubleparked88@gmail.com>
2023-04-30 19:56:28 -0700

    email sender API working

--- server.js
+++ server.js
@@ -4,10 +4,13 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
+// local database
 const mongoose = require('mongoose');
 //bring in employee model to this page to be used
 const Employee = require('./models/employee')
+// email API
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 
 const app = express();
@@ -28,9 +31,11 @@ db.once('open', function() {
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => console.log(`Good listening on PORT: ${PORT}`));
 
-app.get('/test', (request, response) => {
-  response.send('test request received')
-})
+
+
+
+
+app.get('/test', sendEmail)
 
 app.get('/', (req, res)=>{
     res.send('The server is working');
@@ -53,7 +58,25 @@ async function postEmployee(request, response, next){
     }
 }
 
-
+//testing email API
+function sendEmail(req,res,next){
+    const msg = {
+        to: 'doubleparked88@gmail.com', // Change to your recipient
+        from: 'juan.c.olmedo@icloud.com', // Change to your verified sender
+        subject: 'Sending with SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+      }
+    
+      sgMail
+      .send(msg)
+      .then(() => {
+        console.log('Email sent')
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+}
 
 
 
